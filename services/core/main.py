5e98b956432b5ff2521649614fb208f6fb60ae6a
Yeongjin Kim <77034308+7591yj@users.noreply.github.com>
2025-11-02 14:54:09 +0900

    feat(core): add quali result generate

--- services/core/main.py
+++ services/core/main.py
@@ -10,8 +10,19 @@ import boto3
 from dotenv import load_dotenv
 from fastapi.responses import StreamingResponse
 import io
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import pandas as pd
+from timple.timedelta import strftimedelta
 
-load_dotenv('.env')
+import fastf1
+import fastf1.plotting
+from fastf1.core import Laps
+
+import base64
+
+load_dotenv(".env")
 
 app = fastapi.FastAPI(
     title="YaruMotors API", description="API for the YaruMotors project"
@@ -51,7 +62,6 @@ class ManifestQuery(BaseModel):
 class SessionQuery(BaseModel):
     year: int
     event: str
-    session: str
 
 
 # TODO: REMOVE
@@ -82,32 +92,92 @@ def check_manifest(data: ManifestQuery):
     raise HTTPException(status_code=resp.status_code)
 
 
-@app.post("/session/generate", dependencies=[Depends(require_auth)])
-def generate_asset(data: SessionQuery):
+@app.post("/session/race/generate", dependencies=[Depends(require_auth)])
+def generate_race_asset(data: SessionQuery):
     """
     Trigger generation for a specific session
     Payload example:
       {
         "year": 2025,
         "event": "Sao Paulo",
-        "session": "Race",
-        "asset": "constructors"
       }
     """
     try:
-        session = fastf1.get_session(data.year, data.event, data.session)
+        session = fastf1.get_session(data.year, data.event, "R")
         session.load()
         if session.results is None or session.results.empty:
             return {"status": "unavailable"}
         results = session.results[["Abbreviation", "Position", "Time", "Status"]]
         return {
             "status": "ready",
-            "event": f"{data.event} {data.session}",
+            "event": f"{data.event} Race",
             "results": results.to_dict(orient="records"),
         }
     except Exception as e:
         return {"status": "error", "details": str(e)}
 
+@app.post("/session/qualifying/generate", dependencies=[Depends(require_auth)])
+def generate_qualifying_asset(data: SessionQuery):
+    """
+    Trigger generation for a qualifying session result
+    Payload example:
+      {
+        "year": 2025,
+        "event": "Sao Paulo",
+      }
+    """
+    try:
+        session = fastf1.get_session(data.year, data.event, "Q")
+        session.load()
+        if session.results is None or session.results.empty:
+            return {"status": "unavailable"}
+        drivers = pd.unique(session.laps['Driver'])
+        list_fastest_laps = list()
+        for drv in drivers:
+            drvs_fastest_lap = session.laps.pick_drivers(drv).pick_fastest()
+            list_fastest_laps.append(drvs_fastest_lap)
+        fastest_laps = Laps(list_fastest_laps) \
+            .sort_values(by='LapTime') \
+            .reset_index(drop=True)
+        pole_lap = fastest_laps.pick_fastest()
+        fastest_laps['LapTimeDelta'] = fastest_laps['LapTime'] - pole_lap['LapTime']
+        team_colors = list()
+        for index, lap in fastest_laps.iterlaps():
+            color = fastf1.plotting.get_team_color(lap['Team'], session=session)
+            team_colors.append(color)
+
+        fig, ax = plt.subplots()
+        ax.barh(fastest_laps.index, fastest_laps['LapTimeDelta'],
+                color=team_colors, edgecolor='grey')
+        ax.set_yticks(fastest_laps.index)
+        ax.set_yticklabels(fastest_laps['Driver'])
+
+        # show fastest at the top
+        ax.invert_yaxis()
+
+        # draw vertical lines behind the bars
+        ax.set_axisbelow(True)
+        ax.xaxis.grid(True, which='major', linestyle='--', color='black', zorder=-1000)
+
+        lap_time_string = strftimedelta(pole_lap['LapTime'], '%m:%s.%ms')
+
+        plt.suptitle(f"{session.event['EventName']} {session.event.year} Qualifying\n"
+                    f"Fastest Lap: {lap_time_string} ({pole_lap['Driver']})")
+
+        # save the plot to a buffer
+        buf = io.BytesIO()
+        plt.savefig(buf, format='png')
+        buf.seek(0)
+        r2.upload_fileobj(buf, BUCKET, f"{data.year}/{data.event}/qualifying.png")
+        file_url = f"https://{os.getenv('R2_ACCOUNT_ID')}.r2.cloudflarestorage.com/{BUCKET}/{data.year}/{data.event}/qualifying.png"
+        buf.close()
+
+        return {
+            "status": "ready",
+            "url": file_url,
+        }
+    except Exception as e:
+        return {"status": "error", "details": str(e)}
 
 @app.post("/cron/run", dependencies=[Depends(require_auth)])
 def cron_run():
