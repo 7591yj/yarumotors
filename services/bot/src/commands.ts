b1ad519b74237e31eca7680f11b61ac4a71a5adb
Yeongjin Kim <77034308+7591yj@users.noreply.github.com>
2025-11-04 17:54:34 +0900

    feat(bot): start hono with binds; add /ping

---
+++ services/bot/src/commands.ts
@@ -0,0 +1,6 @@
+export const PING_COMMAND = {
+  name: "ping",
+  description: "Check latency stats of the bot.",
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+};
--- services/bot/src/index.ts
+++ services/bot/src/index.ts
@@ -1,7 +1,10 @@
 import { Hono } from "hono";
+import { Bindings } from "./configuration";
 
-const app = new Hono();
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.get("/", (c) => c.text("Hello Cloudflare Workers!"));
+app.get("/", (c) => c.text("Yarumotors is Running."));
+
+app.all("*", (c) => c.text("Not Found.", { status: 404 }));
 
 export default app;
